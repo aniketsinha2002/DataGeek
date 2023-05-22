a3c3fef59829057a0d5a76a9a316c83a8b74f2cb
Ishita <ishitasrivastava.salwan@gmail.com>
2023-05-22 07:50:42 +0530

    Synchronized Number counter animation

--- main.js
+++ main.js
@@ -50,3 +50,56 @@ const closeNav = () => {
 }
 
 closeBtn.addEventListener('click', closeNav)
+
+
+//Number counter animation
+function animateCounter(counterElement, targetNumber, duration) {
+    const startNumber = parseInt(counterElement.innerText);
+    let increment = Math.ceil((targetNumber - startNumber) / (duration / 100)); 
+    let d_interval= 60;
+
+    console.log(targetNumber)
+
+    if(targetNumber<100){
+      increment=1;
+      d_interval= 115;
+    }
+
+    console.log(increment);
+
+
+    let currentNumber = startNumber;
+    const timer = setInterval(() => {
+      currentNumber += increment; 
+      counterElement.innerText = currentNumber+"+"; 
+
+      // Check if the current number has reached or exceeded the target number
+      if ((increment > 0 && currentNumber >= targetNumber) || (increment < 0 && currentNumber <= targetNumber)) {
+        clearInterval(timer);
+        counterElement.innerText = targetNumber+"+"; 
+      }
+    }, d_interval); 
+    return timer; 
+  }
+
+  function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+      const counterElement = entry.target;
+      const targetNumber = parseInt(counterElement.dataset.target);
+      const duration = 5000; 
+      if (entry.isIntersecting) {
+        counterElement.style.visibility = 'visible'; 
+        animateCounter(counterElement, targetNumber, duration);
+        observer.unobserve(counterElement); 
+      } else {
+        counterElement.style.visibility = 'hidden';
+      }
+    });
+  }
+
+  //using IntersectionObserver to triggrt animation once element is visible on screen
+  const observer = new IntersectionObserver(handleIntersection);
+  const counters = document.querySelectorAll('.num_count');   
+  counters.forEach((counterElement) => {
+    observer.observe(counterElement); // Observe each counter element
+  });
