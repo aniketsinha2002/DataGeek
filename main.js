24b2537cf22b3b1a2f43080a37559231b35f6699
Premsai Vamra <premsaivarmacoding@gmail.com>
2023-05-22 18:51:27 +0530

    added functionalities to the navbar

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Welcome !!");
+// alert("Welcome !!");
 
 // change navbar styles on scroll
 
@@ -18,7 +18,7 @@ faqs.forEach(faq => {
 
         // change icon
         const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus') {
+        if (icon.className === 'uil uil-plus') {
             icon.className = "uil uil-minus";
         } else {
             icon.className = "uil uil-plus";
@@ -28,25 +28,56 @@ faqs.forEach(faq => {
 })
 
 
-
+// New Code 
 // show/hide nav menu
-const menu = document.querySelector(".nav__menu");
-const menuBtn = document.querySelector("#open-menu-btn");
-const closeBtn = document.querySelector("#close-menu-btn");
+const burgerIcon = document.querySelector('#open-menu-btn');
+const navItems = document.querySelector('.nav_items');
+const navMenu = document.querySelector('.nav_menu');
+const container = document.querySelector('.container');
+burgerIcon.classList.add('uil-bars');
 
+burgerIcon.addEventListener('click', () => {
+    burgerIcon.classList.toggle('uil-bars');
+    if (navItems.style.display === 'flex') {
+        navItems.style.display = 'none';
+        container.style.paddingTop = '6rem';
+    } else {
+        navItems.style.display = 'flex';
+        container.style.paddingTop = '12rem';
+    }
+    burgerIcon.classList.toggle('uil-multiply');
+});
 
-menuBtn.addEventListener('click', () => {
-    menu.style.display = "flex";
-    closeBtn.style.display = "inline-block";
-    menuBtn.style.display = "none";
+document.body.addEventListener('scroll', () => {
+    if (document.body.scrollTop <= 0) {
+        console.log('Scrolling Down')
+    } else {
+        burgerIcon.classList.remove('uil-multiply');
+        burgerIcon.classList.add('uil-bars');
+        navItems.style.display = 'none';
+    }
 })
 
 
-// close nav menu 
-const closeNav = () => {
-    menu.style.display = "none";
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline-block";
-}
 
-closeBtn.addEventListener('click', closeNav)
+// // Old Code
+// const menu = document.querySelector(".nav__menu");
+// const menuBtn = document.querySelector("#open-menu-btn");
+// const closeBtn = document.querySelector("#close-menu-btn");
+
+
+// menuBtn.addEventListener('click', () => {
+//     menu.style.display = "flex";
+//     closeBtn.style.display = "inline-block";
+//     menuBtn.style.display = "none";
+// })
+
+
+// // close nav menu 
+// const closeNav = () => {
+//     menu.style.display = "none";
+//     closeBtn.style.display = "none";
+//     menuBtn.style.display = "inline-block";
+// }
+
+// closeBtn.addEventListener('click', closeNav)
