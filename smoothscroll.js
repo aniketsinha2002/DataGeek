17251c253c18b72d239c413367bc370eca0a0144
Shreya Gour <78635373+Shreyagour@users.noreply.github.com>
2023-05-29 16:45:41 +0530

    Delete smoothscroll.js

--- smoothscroll.js
+++
@@ -1,6 +0,0 @@
-$('nav ul li a').click(function(){
-    var thisSection=$(this).attr('href');
-     $('html, body').stop().animate({
-    scrollTop:$(thisSection).offset().top-140
-    },600,'easeOutCirc');
-});
